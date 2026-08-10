df09fd7062a9601ad5daf8520cefd48634d1d9a0
Asmamew Ademasu <nsr.122.15@amu.edu.et>
2026-08-10 21:59:11 +0300

    applay RLS on database by changeing anon key by service role key

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const supabaseClient = createClient(
   process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_ANON_KEY as string
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
 
 export default supabaseClient;
